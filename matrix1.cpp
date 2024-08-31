3b7c7cda34801fbecc1b0fd22eb25a74c6391f46
PalashMerwana <palash.merwana.btech2023@sitpune.edu.in>
2024-08-31 12:13:53 +0530

    Add files via upload

---
+++ matrix1.cpp
@@ -0,0 +1,27 @@
+//Palash Merwana 
+//Prn: 23070123095
+// experiment 8
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int temp[3][3], i, j;
+    
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            cout << "Enter element [" << i << "][" << j << "]: ";
+            cin >> temp[i][j];
+        }
+    }
+    
+    cout << "The 3x3 matrix is: " << endl;
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            cout << temp[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
---
+++ matrix_diagonal_addition.cpp
@@ -0,0 +1,24 @@
+//Palash Merwana 
+//Prn: 23070123095
+// experiment 8
+
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int mat[4][4] = {{1, 2, 3, 4},  
+                     {5, 6, 7, 8},  
+                     {1, 2, 3, 4},  
+                     {5, 6, 7, 8}};
+
+    int principal = 0;
+    
+    for (int i = 0; i < 4; ++i) 
+        principal += mat[i][i]; 
+
+    cout << "Sum of the diagonal elements is: " << principal << endl;
+
+    return 0;
+}
